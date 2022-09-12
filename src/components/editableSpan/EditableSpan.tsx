f46efcef6fc9501649cb20e6caf1c63e364b33b5
EnzoK <kropotin_aleksey@mail.ru>
2022-09-12 20:40:14 +0300

    feat: add style from EditableSpan.tsx

--- src/components/editableSpan/EditableSpan.tsx
+++ src/components/editableSpan/EditableSpan.tsx
@@ -2,6 +2,9 @@ import * as React from 'react';
 import { useState } from 'react';
 
 import { EditFilled } from '@ant-design/icons';
+import { Button, Input } from 'antd';
+
+import s from './editableSpan.module.css';
 
 type EditableSpanType = {
   callBack: (name: string, avatar?: string) => void;
@@ -10,28 +13,30 @@ type EditableSpanType = {
 
 export const EditableSpan: React.FC<EditableSpanType> = ({ callBack, title }) => {
   const [editMode, setEditMode] = useState(false);
+  const [value, setValue] = useState<string>(title);
 
   const onClickDoubleHandler: () => void = () => {
+    setValue(title);
     setEditMode(!editMode);
   };
-  const onClickHandler: () => void = () => {
-    callBack('inputValue');
+  const handleClick: () => void = () => {
+    callBack(value);
     onClickDoubleHandler();
   };
 
   return (
-    <div>
+    <div className={s.wrapper}>
       {!editMode ? (
         <div onDoubleClick={onClickDoubleHandler}>
           <span>{title}</span>
           <EditFilled style={{ width: '20px', height: '20px' }} />
         </div>
       ) : (
-        <div>
-          <input type="text" />
-          <button type="button" onClick={onClickHandler}>
-            submit
-          </button>
+        <div className={s.group}>
+          <Input bordered={false} autoFocus className={s.input} value={value} />
+          <Button size="small" className={s.btn} onClick={handleClick}>
+            save
+          </Button>
         </div>
       )}
     </div>
