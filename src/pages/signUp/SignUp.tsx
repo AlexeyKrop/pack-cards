eb7f19d0b76aa8dbee537f8fb55ade675e2205de
EnzoK <kropotin_aleksey@mail.ru>
2022-09-12 11:50:59 +0300

    feat: add signUp.module.css

--- src/pages/signUp/SignUp.tsx
+++ src/pages/signUp/SignUp.tsx
@@ -2,17 +2,25 @@ import React from 'react';
 
 import { yupResolver } from '@hookform/resolvers/yup';
 import { Input } from 'antd';
-import { SubmitHandler, useForm, Controller } from 'react-hook-form';
+import { Controller, SubmitHandler, useForm } from 'react-hook-form';
 import * as yup from 'yup';
+
+import s from './signUp.module.css';
 
 type IFormInput = {
   email: string;
   password: string;
+  confirm_password: string;
 };
 const schema = yup
   .object({
     email: yup.string().required(),
     password: yup.string().required(),
+    confirm_password: yup
+      .string()
+      .label('confirm password')
+      .required()
+      .oneOf([yup.ref('password'), null], 'Passwords must match'),
   })
   .required();
 
@@ -30,22 +38,56 @@ export const SignUp: React.FC = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <Controller
-        name="email"
-        control={control}
-        defaultValue=""
-        render={({ field }) => <Input placeholder="email" {...field} />}
-      />
-      <p>{errors.email?.message}</p>
-      <Controller
-        name="password"
-        control={control}
-        defaultValue=""
-        render={({ field }) => <Input.Password placeholder="input password" {...field} />}
-      />
-      <p>{errors.password?.message}</p>
-      <input type="submit" />
-    </form>
+    <div className={s.wrapper}>
+      <div className={s.content}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <h2>Sing Up</h2>
+          <div className={s.input}>
+            <Controller
+              name="email"
+              control={control}
+              defaultValue=""
+              render={({ field }) => (
+                <Input status={errors.email && 'error'} placeholder="email" {...field} />
+              )}
+            />
+            <p className={s.error}>{errors.email?.message}</p>
+          </div>
+          <div className={s.input}>
+            <Controller
+              name="password"
+              control={control}
+              defaultValue=""
+              render={({ field }) => (
+                <Input.Password
+                  status={errors.password && 'error'}
+                  placeholder="password"
+                  {...field}
+                />
+              )}
+            />
+            <p className={s.error}>{errors.password?.message}</p>
+          </div>
+          <div className={s.input}>
+            <Controller
+              name="confirm_password"
+              control={control}
+              defaultValue=""
+              render={({ field }) => (
+                <Input.Password
+                  status={errors.confirm_password && 'error'}
+                  placeholder="confirm password"
+                  {...field}
+                />
+              )}
+            />
+            <p className={s.error}>{errors.confirm_password?.message}</p>
+          </div>
+          <input className={s.inputBtn} type="submit" value="Sign Up" />
+          <p className={s.text}> Already have an account?</p>
+          <div className={s.link}>Sing In</div>
+        </form>
+      </div>
+    </div>
   );
 };
