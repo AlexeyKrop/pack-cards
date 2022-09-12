63c1f2bb2512b1f40a0401222d0988806e67ca9d
EnzoK <kropotin_aleksey@mail.ru>
2022-09-12 13:28:58 +0300

    feat: add Login.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 
 import './App.css';
+import { Login } from './pages/login/Login';
 import { SignUp } from './pages/signUp/SignUp';
 
 const App: React.FC = () => {
-  return <SignUp />;
+  return (
+    <>
+      {' '}
+      <SignUp />
+      <Login />
+    </>
+  );
 };
 
 export default App;
--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,5 +1,78 @@
 import React from 'react';
 
+import { yupResolver } from '@hookform/resolvers/yup/dist/yup';
+import { Input } from 'antd';
+import { Controller, SubmitHandler, useForm } from 'react-hook-form';
+import * as yup from 'yup';
+
+import s from '../signUp/signUp.module.css';
+
+type IFormInput = {
+  email: string;
+  password: string;
+  checkbox: boolean;
+};
+const schema = yup
+  .object({
+    email: yup.string().required(),
+    password: yup.string().required(),
+  })
+  .required();
+
 export const Login: React.FC = () => {
-  return <div />;
+  const {
+    register,
+    control,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<IFormInput>({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit: SubmitHandler<IFormInput> = data => {
+    console.log(data);
+  };
+
+  return (
+    <div className={s.wrapper}>
+      <div className={s.content}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <h2>Sign in</h2>
+          <div className={s.input}>
+            <Controller
+              name="email"
+              control={control}
+              defaultValue=""
+              render={({ field }) => (
+                <Input status={errors.email && 'error'} placeholder="email" {...field} />
+              )}
+            />
+            <p className={s.error}>{errors.email?.message}</p>
+          </div>
+          <div className={s.input}>
+            <Controller
+              name="password"
+              control={control}
+              defaultValue=""
+              render={({ field }) => (
+                <Input.Password
+                  status={errors.password && 'error'}
+                  placeholder="password"
+                  {...field}
+                />
+              )}
+            />
+            <p className={s.error}>{errors.password?.message}</p>
+          </div>
+          <div className={s.checkbox}>
+            <input type="checkbox" {...register('checkbox')} />
+            <span>Remember me</span>
+          </div>
+          <input className={s.inputBtn} type="submit" value="Sign In" />
+          <p className={s.text}>You don&apos;t have an account?</p>
+          <div className={s.link}>Sing Up</div>
+        </form>
+      </div>
+    </div>
+  );
 };
