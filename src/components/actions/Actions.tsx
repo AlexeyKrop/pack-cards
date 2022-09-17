0e542563a0d5e7c3243289e5cb8518529f3ff646
EnzoK <kropotin_aleksey@mail.ru>
2022-09-17 20:54:48 +0300

    fix: style for filter

--- src/components/actions/Actions.tsx
+++ src/components/actions/Actions.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 
 import { DeleteOutlined, EditOutlined, ReadOutlined } from '@ant-design/icons';
+import { Button } from 'antd';
 
 import s from './actions.module.css';
 
 export const Actions: React.FC = () => {
   return (
     <div className={s.wrapper}>
-      <ReadOutlined className={s.icon} />
-      <EditOutlined className={s.icon} />
-      <DeleteOutlined className={s.icon} />
+      <Button type="text" icon={<ReadOutlined className={s.icon} />} size="middle" />
+      <Button type="text" icon={<EditOutlined className={s.icon} />} size="middle" />
+      <Button type="text" icon={<DeleteOutlined className={s.icon} />} size="middle" />
     </div>
   );
 };
