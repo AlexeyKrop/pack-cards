7f57600b606e8a7c7d9434f94c08f10692d668d2
EnzoK <kropotin_aleksey@mail.ru>
2022-09-19 19:32:33 +0300

    feat: add Radio.Group

--- src/components/toggleButton/ToggleButton.tsx
+++ src/components/toggleButton/ToggleButton.tsx
@@ -1,6 +1,6 @@
-import React, { MouseEvent } from 'react';
+import React, { useState } from 'react';
 
-import { Radio } from 'antd';
+import { Radio, RadioChangeEvent } from 'antd';
 
 import { useAppDispatch } from '../../hooks/useAppDispatch/useAppDispatch';
 import { useAppSelector } from '../../hooks/useAppSelector/useAppSelector';
@@ -11,39 +11,46 @@ import { SkeletonButton } from '../skeletonButton/SkeletonButton';
 type ToggleButtonType = {
   disabled: boolean;
 };
+const options = [
+  {
+    label: 'My',
+    value: 'My',
+  },
+  {
+    label: 'All',
+    value: 'All',
+  },
+];
+
 export const ToggleButton: React.FC<ToggleButtonType> = ({ disabled }) => {
   const user = useAppSelector(selectUser);
   const dispatch = useAppDispatch();
   const userID = user._id;
+  const [btnValue, setBtnValue] = useState<string>('All');
 
-  const onClickHandle: (e: MouseEvent<HTMLElement>) => void = e => {
-    const btnId = e.currentTarget.id;
-
-    if (btnId === 'myPack') {
+  const onChange: (e: RadioChangeEvent) => void = ({ target: { value } }) => {
+    if (value === 'My') {
       dispatch(setMyCardsPack({ id: userID }));
     }
-    if (btnId === 'allPack') {
+    if (value === 'All') {
       dispatch(setMyCardsPack({ id: '' }));
     }
+    setBtnValue(value);
   };
 
   if (disabled) {
-    return <SkeletonButton width={150} active size="large" />;
+    return <SkeletonButton width={200} active size="large" />;
   }
 
   return (
-    <Radio.Group defaultValue="all" buttonStyle="solid" size="large">
-      <Radio.Button
-        id="myPack"
-        onClick={onClickHandle}
-        value="my"
-        style={{ margin: '0 40px 0 0' }}
-      >
-        My
-      </Radio.Button>
-      <Radio.Button id="allPack" value="all" onClick={onClickHandle}>
-        All
-      </Radio.Button>
-    </Radio.Group>
+    <Radio.Group
+      size="large"
+      defaultValue="All"
+      options={options}
+      onChange={onChange}
+      value={btnValue}
+      optionType="button"
+      buttonStyle="solid"
+    />
   );
 };
